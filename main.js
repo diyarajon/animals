faeedff2833b0d816f055f0896293d7b8ffb6a9b
Diya <diya.rajon@yahoo.com>
2020-04-12 17:43:55 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
+var pageCounter = 1;
 var animalContainer = document.getElementById("animal-info");
 var btn = document.getElementById("btn");
 
 btn.addEventListener("click", function()
 {
     var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
     ourRequest.onload = function(){
         var ourData = JSON.parse(ourRequest.responseText);
         renderHTML(ourData);
     };
     ourRequest.send();
+    pageCounter++;
 });
 
 
@@ -17,7 +19,7 @@ function renderHTML(data){
     var htmlString = "";
 
     for(i = 0; i < data.length; i++){
-        htmlString += "<p>" + data[i].name + "is a " + data[i].species + ".</p>";
+        htmlString += "<p>" + data[i].name + " is a " + data[i].species + ".</p>";
     }
 
     animalContainer.insertAdjacentHTML('beforeend', htmlString);
