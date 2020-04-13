fd08b9eb14c8d4a1e8ab1aa42a213cb48aa4f3ab
Diya <diya.rajon@yahoo.com>
2020-04-12 22:48:55 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-
+var pageCounter = 1;
 var animalContainer = document.getElementById("animal-info");
 var btn = document.getElementById("btn");
 
+btn.addEventListener("click", function()
+{
     var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'https://diyarajon.github.io/cats/');
+    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
     ourRequest.onload = function(){
         if (ourRequest.status >= 200 && ourRequest.status < 400){
             var ourData = JSON.parse(ourRequest.responseText);
@@ -16,7 +18,13 @@ var btn = document.getElementById("btn");
     ourRequest.onerror = function(){
         console.log("Connection error");
     };
+
     ourRequest.send();
+    pageCounter++;
+    if (pageCounter > 3){
+        //btn.classList.add("hide-me");
+    }
+});
 
 
 function renderHTML(data){
